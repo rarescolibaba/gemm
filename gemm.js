cee759975a1303cecd911b2072ccb27bfa522bec
Rareș-Andrei Colibaba <72991062+rarescolibaba@users.noreply.github.com>
2021-07-15 15:20:03 +0000

    Clean up sfx, add simple file loader, randomize played sound effect

--- gemm.js
+++ gemm.js
@@ -1,32 +1,61 @@
 // https://kaboomjs.com/#kaboom
 const k = kaboom({
-  global: true,
-  width: 800,
-  height: 400,
+	global: true,
+	width: 800,
+	height: 400,
 	clearColor: false,
 	scale: 1,
 	debug: true, // https://kaboomjs.com/#debug
 	crisp: true,
-	plugins: [], // hopefully doesn't overwrite
+	plugins: [loadThis],
 }), z = new vec2(width(), height())
 
 // Shadow offset
-const sOff = new vec2(0, z.y/100)
+const sOff = new vec2(0, z.y / 100)
+let samples = {}
 
-//TODO: Trim audio files so they start right at the sound effect
-loadSound("click1", "click1.ogg")
-loadSound("click2", "click2.ogg")
-loadSound("click3", "click3.ogg")
-loadSound("click4", "click4.ogg")
-// I really should find a better method for this
-const clickSounds = ["click1", "click2", "click3", "click4"]
+function loadThis(k) {
+	return {
+		// Usage: loadThis("path/to/click.ogg", upto = 4") to load click1.ogg - click4.ogg as "click1" - "click4"
+		loadThis(file, upto = 1, i = 1, name = null) {
+			let parts = file.split(".")
+			let type = parts.pop()
+			file = parts
+			if (!name) name = parts.join().split("/").pop()
+			switch (type) {
+				case "ogg":
+				case "mp3":
+				case "wav":
+					for (i; i <= upto; i++)
+						loadSound(`${name}${i}`, `${file}${i}.${type}`)
+					break
+				case "jpg":
+				case "jpeg":
+				case "png":
+				case "img":
+					for (i; i <= upto; i++)
+						loadSprite(`${name}${i}`, `${file}${i}`)
+					break
+				case "pedit":
+					for (i; i <= upto; i++)
+						loadPedit(`${name}${i}`, `${file}${i}`)
+				default:
+					throw new Error(`unknown file type "${type}"`)
+			}
+			return upto
+		}
+	}
+}
+
+// Track how many samples of each sound effect there are. Find a better method to pick a random sample?
+samples[clicks] = loadThis("audio/click.ogg", upto = 4)
 
 scene("mainmenu", () => {
 	add([text("gemm!", 128), origin("top"), color(0), pos(400, 14), "shadow"])
-  const title = add([text("gemm!", 128), origin("top"), pos(400, 10), , "title"])
+	const title = add([text("gemm!", 128), origin("top"), pos(400, 10), "title"])
 
 	add([rect(480, 30), color(0), pos(96, 164), "shadow"])
-  add([rect(480, 30), pos(96, 160), "title"])
+	add([rect(480, 30), pos(96, 160), "title"])
 
 	add([
 		text("A minigame where you farm gemmz to earn", 12),
@@ -37,6 +66,8 @@ scene("mainmenu", () => {
 	])
 
 	// Main menu effects
+	mouseClick(() => camShake(1))
+
 	let mouseToggle = false
 	title.clicks(() => {
 		if (!mouseToggle) {
@@ -44,23 +75,23 @@ scene("mainmenu", () => {
 			every("title", (obj) => {
 				obj.move(0, 240)
 			})
-			play(choose(clickSounds)) // Play around with pitch
+			play(`click${Math.round(rand(samples[clicks]))}`) // Play around with pitch
 			// Transition: slide upward to black
 			wait(0.5, () => {
 				every("title", (obj) => {
 					obj.move(0, -240)
 				})
+				play(`click${Math.round(rand(samples[clicks]))}`)
 			})
 			wait(1.5, () => {
-				go("first")
+				go("level1")
 			})
-			// Load in assets here? (test from "first") Find a better method (loop)
-			camShake(2)
+			// Load assets for level1 here
 		}
 	})
 })
 
-scene("first", () => {
+scene("level1", () => {
 	// Start with black screen
 	console.log("loaded \"first\"")
 })
